5f9f743f58dbe3ead72a141fff4b915b8e148547
Jayce <jfengg3@gmail.com>
2022-02-28 23:11:30 +0800

    updated w hashmap

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.lang.Math;
 
 public class Main {
@@ -10,6 +12,9 @@ public class Main {
         Q1(obj);
         Q2(obj);
         Q3(obj);
+        Q4(obj);
+        Q5(obj);
+        Q6(obj);
 
     }
 
@@ -61,7 +66,70 @@ public class Main {
         /* After */
         System.out.println("Search a number: ");
         int idx = FindValue(LinkedList, input.nextInt());
-        System.out.println("Random generated number is found in index " + idx);
+        System.out.println("Input number is found in index " + idx);
+    }
+
+    public static void Q4(Scanner input) {
+
+        HashMap<Integer, Integer> HashMap = new HashMap<Integer, Integer>();
+        HashMap.put(0, 1);
+        HashMap.put(1, 3);
+        HashMap.put(2, 5);
+        HashMap.put(3, 7);
+        HashMap.put(4, 9);
+        HashMap.put(5, 11);
+
+        /* Before */
+        for (int i = 0; i < HashMap.size(); i++) {
+            System.out.print(HashMap.get(i) + " ");
+        }
+        /* After */
+        System.out.println("Insert a number into HashMap: \n");
+        AddAndSortHM(HashMap, input.nextInt());
+        for (int i = 0; i < HashMap.size(); i++) {
+            System.out.print(HashMap.get(i) + " ");
+        }
+    }
+
+    public static void Q5(Scanner input) {
+
+        HashMap<Integer, Integer> HashMap = new HashMap<Integer, Integer>();
+        HashMap.put(0, 1);
+        HashMap.put(1, 3);
+        HashMap.put(2, 5);
+        HashMap.put(3, 7);
+        HashMap.put(4, 9);
+        HashMap.put(5, 11);
+
+        /* Before */
+        for (int i = 0; i < HashMap.size(); i++) {
+            System.out.print(HashMap.get(i) + " ");
+        }
+        /* After */
+        System.out.println("Swap 2 indexes in the HashMap: \n");
+        SwapHM(HashMap, input.nextInt(), input.nextInt());
+        for (int i = 0; i < HashMap.size(); i++) {
+            System.out.print(HashMap.get(i) + " ");
+        }
+    }
+
+    public static void Q6(Scanner input) {
+
+        HashMap<Integer, Integer> HashMap = new HashMap<Integer, Integer>();
+
+        for (int i = 0; i < 500; i++) {
+            HashMap.put(i, (int) (Math.random() * (9999 - 1000 + 1)) + 1000);
+        }
+
+        /* Before */
+        for (int i = 0; i < HashMap.size(); i++) {
+            System.out.print(HashMap.get(i) + " ");
+        }
+        /* After */
+        System.out.println("Search a number: ");
+        int idx = FindValueHM(HashMap, input.nextInt());
+        System.out.println("Input number is found in index " + idx);
+
     }
 
     public static void AddAndSort(LinkedList<Integer> list, int value) {
@@ -103,6 +171,41 @@ public class Main {
         } else {
             return -1;
         }
+
+    }
+
+    public static void AddAndSortHM(HashMap<Integer, Integer> hashmap, int value) {
+
+        hashmap.put(hashmap.size(), value);
+
+        ArrayList<Integer> hmlist = new ArrayList<>(hashmap.values());
+        hmlist.sort((a, b) -> a.compareTo(b));
+
+        for (int i = 0; i < hashmap.size(); i++) {
+            hashmap.put(i, hmlist.get(i));
+        }
+
+    }
+
+    public static void SwapHM(HashMap<Integer, Integer> hashmap, int value, int value2) {
+
+        int idx1 = hashmap.get(value);
+        int idx2 = hashmap.get(value2);
+        hashmap.put(value, idx2);
+        hashmap.put(value2, idx1);
+    }
+
+    public static int FindValueHM(HashMap<Integer, Integer> hashmap, int value) {
+
+        for (int i = 0; i < hashmap.size(); i++) {
+
+            if (hashmap.get(i) == value) {
+                return i;
+            }
+
+        }
+
+        return -1;
 
     }
 
